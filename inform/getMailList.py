a7b883be40ccc66869b76c19bc24b0df3e5a771b
dongha <64480531+DHki@users.noreply.github.com>
2023-04-12 04:39:05 +0000

    initial commit

---
+++ inform/getMailList.py
@@ -0,0 +1,8 @@
+### get users' mail from database ###
+
+
+### the function returns recievers list with database table name as params ###
+def get_recievers(table):
+    recieversList = []
+    
+    return recieversList
---
+++ inform/sendMail.py
@@ -0,0 +1,20 @@
+### sending mail to users.
+### this file get users' mail address from getMailList.py with name of websites
+
+import smtplib
+from email.mime.text import MIMEText
+
+### the function sends mail with class object, recievers as params ###
+### classObj has mail subject, content, and link ###
+def send_mail(classObj, recievers):
+    smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpObj.starttls()
+
+    smtpObj.login('kdh101800@gmail.com', 'fpie jqjn xgza cibl')
+    
+    mailContent = '<p>' + classObj.content + '</p>' + '<a href="' + classObj.link + '">' + classObj.link + '</a>'
+    msg = MIMEText(mailContent, 'html')
+    msg['Subject'] = classObj.subject
+    
+    ### recievers, second parameter, can send more than one if recievers is the list of string ###
+    smtpObj.sendmail('kdh101800@gmail.com', recievers, msg.as_string())

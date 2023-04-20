0a3d9e588da0e05702572040e7bf4d410d78bbf4
DHki <kdh101800@gmail.com>
2023-04-20 11:09:27 +0900

    add database connect

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import crawling
 
 
 def main():
-    link = crawling.hyu_cs_board_crawling()
+    # 크롤링의 결과값 저장 (False or websitename, url, posts : list) #
+    checkResult = crawling.check_script()
     
-    if link != False:
-        noticeObj = inform.Notice()
-        noticeObj.make_script('content for combine', '')
-        noticeObj.make_title('the title', 'somewebsite')
+    for result in checkResult:
+        if result is False:
+            continue
         
-        recievers = ['kdh101800@gmail.com', 'ken1934@naver.com']
+        # Notice object 생성, 내용 만들기 #
+        script = inform.Notice()
+        script.make_script(result[2], result[1])
+        script.make_title(result[2], result[0])
         
-        inform.send_mail(noticeObj, recievers)
+        # 해당 웹사이트에서 알림을 받는 사람들 추출#
+        recievers = inform.get_recievers(result[0])
+        # 메일 전송 #
+        inform.send_mail(script, recievers)
         
-
+        
 if __name__ == '__main__':
     main()

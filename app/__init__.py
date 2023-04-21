84ac0959c790698afbfac606ed8ba5752f9350cb
DHki <kdh101800@gmail.com>
2023-04-21 16:13:57 +0900

    feat: [quick_work api] add, delete, get to/from email_list

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, request
+from flask_restx import Api, Resource
 
-def create_app():
-    app = Flask(__name__)
 
-    @app.route('/')
-    def index():
-        return render_template('index.html')
+from . import connectDB
+from .conf import DB_ID, DB_PWD, DB_NAME, sites
 
-    return app
+app = Flask(__name__)
+api = Api(app)
+
+@api.route('/quick_work')
+class addEmail(Resource):
+    
+    connectDB.connect(DB_ID, DB_PWD, DB_NAME)
+    
+    def post(self):
+        # insert to database user email #
+        
+        data = request.json.get('data')
+        
+        if connectDB.push_email(data['user'], data['email'], data['website']):
+            return {
+                'post' : 'success'
+            }
+        else:
+            return{
+                'post' : 'fail'
+            }
+    
+    def delete(self):
+        # drop row from email_list #
+        
+        data = request.json.get('data')
+        
+        if connectDB.delete_email(data['user'], data['email'], data['website']):
+            return {
+                'delete' : 'success'
+            }
+        else:
+            return{
+                'delete' : 'fail'
+            }
+        
+    def get(self):
+        # get list of providing websites #
+        
+        ret = {}
+        for site in sites:
+            ret[site['name']] = site['url']
+            
+        return ret
+        

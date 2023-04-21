493856dd16d326bb16e55fb7638cc3c902b86421
DHki <kdh101800@gmail.com>
2023-04-21 17:17:37 +0900

    feat: module connectDB

--- app/connectDB/working.py
+++ app/connectDB/working.py
@@ -10,9 +10,10 @@ def push_email(user, email, website):
     try:
         cursor = conn.cursor()
                 
-        sql = "INSERT INTO email_list (user_id, email, website_name) VALUES (%s, %s, %s)" %user, email, website
+        sql = "INSERT INTO email_list (user_id, email, website_name) VALUES ('%s', '%s', '%s')" % (user, email, website)
         cursor.execute(sql)
-                
+        
+        conn.commit()
         cursor.close()
     except:
         return False
@@ -23,9 +24,10 @@ def delete_email(user, email, website):
     try:
         cursor = conn.cursor()
             
-        sql = "DELETE FROM email_list where user_id = %s && email = %s && website_name = %s" %user, email, website
+        sql = "DELETE FROM email_list where user_id = '%s' && email = '%s' && website_name = '%s'" % (user, email, website)
         cursor.execute(sql)
-            
+        
+        conn.commit()
         cursor.close()
     except:
         return False

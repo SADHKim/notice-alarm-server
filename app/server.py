b0ac538468f2608028eca7ed467ae6e6df2b5cc2
DHki <kdh101800@gmail.com>
2023-04-22 14:27:55 +0900

    feat: style basic server

--- app/server.py
+++ app/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, session, request, render_template, redirect, url_for
 from flask_restx import Api, Resource
 
 
@@ -8,8 +8,50 @@ from conf import DB_ID, DB_PWD, DB_NAME, sites
 app = Flask(__name__)
 api = Api(app)
 
-@api.route('/quick_work')
-class addEmail(Resource):
+@app.route('/')
+def main():
+    info = {}
+    if session['user'] is not None:
+        info['user'] = session['user']
+        info['email'] = session['email']
+        
+    return render_template('main.html', info=info)
+
+@app.route('/profile')
+def profile():
+    if session['user'] is None:
+        return render_template('not_login.html')
+    else:
+        info = connectDB.get_user_info(session['user'])
+        
+        return render_template('profile.html', info=info)
+        
+@app.route('/register', methods = ['GET', 'POST'])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    
+    elif request.method == 'POST':
+        info = {}
+        
+        info['id'] = request.form['id']
+        info['passwd'] = request.form['passwd']
+        info['email'] = request.form['email']
+        
+        if connectDB.push_user(info):
+            return redirect(url_for('main'))
+        else:
+            return redirect(url_for('error', code = 500))
+
+
+
+
+
+
+
+### API server requests post, delete with json file ###
+@api.route('/api/email')
+class Email(Resource):
     
     connectDB.connect(DB_ID, DB_PWD, DB_NAME)
     
@@ -40,7 +82,9 @@ class addEmail(Resource):
             return{
                 'delete' : 'fail'
             }
-        
+
+@api.route('/api/websites')
+class Website(Resource):
     def get(self):
         # get list of providing websites #
         
@@ -49,7 +93,7 @@ class addEmail(Resource):
             ret[site['name']] = site['url']
             
         return ret
-
+        
 
 def start():
     app.run()

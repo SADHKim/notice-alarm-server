c87f29f880f0fed24d3b9673788ec1a68ff659b3
DHki <kdh101800@gmail.com>
2023-05-09 17:38:17 +0900

    feat: exception handling, schedule

--- app/program.py
+++ app/program.py
@@ -7,30 +7,36 @@ import crawling
 
 def search_pages():
     # 크롤링의 결과값 저장 (False or websitename, url, posts : list) #
-    checkResult = crawling.checking()
-    
-    for result in checkResult:
-        if result is False:
-            continue
+    try:
+        checkResult = crawling.checking()
         
-        # 해당 웹사이트에서 알림을 받는 사람들 추출#
-        recievers = mail.get_recievers(result['name'])
-        # recievers가 없다면 continue #
-        if recievers is False:
-            continue
-        
-        # Notice object 생성, 내용 만들기 #
-        script = mail.Script()
-        script.make_script(result['posts'], result['url'])
-        script.make_title(result['posts'], result['name'])
-        
-        # 메일 전송 #
-        mail.send_mail(script, recievers)
+        for result in checkResult:
+            if result is False:
+                continue
+            
+            # 해당 웹사이트에서 알림을 받는 사람들 추출#
+            recievers = mail.get_recievers(result['name'])
+            # recievers가 없다면 continue #
+            if recievers is False:
+                continue
+            
+            # Notice object 생성, 내용 만들기 #
+            script = mail.Script()
+            script.make_script(result['posts'], result['url'])
+            script.make_title(result['posts'], result['name'])
+            
+            # 메일 전송 #
+            mail.send_mail(script, recievers)
+    except:
+        return
         
         
 def start():
     search_pages()
-    schedule.every(2).hours.do(search_pages)
+    
+    schedule.every().day.at('09:00').do(search_pages)
+    schedule.every().day.at('15:00').do(search_pages)
+    schedule.every().day.at('21:00').do(search_pages)
     
     while True:
         schedule.run_pending()

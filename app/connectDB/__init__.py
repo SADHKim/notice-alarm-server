fab098d5e954cf64a3e49c7084be89e67dfca8d3
DHki <kdh101800@gmail.com>
2023-05-19 11:25:43 +0900

    fix: connectDB module optimization

--- app/connectDB/__init__.py
+++ app/connectDB/__init__.py
@@ -1,6 +1,14 @@
+# init functions
 from .functions import connect, disconnect
+# functions of email_list
 from .functions import push_email, delete_email, get_recievers
+# functions of users
 from .functions import push_user, update_password, update_email, get_user_info, id_overlap_check, login, find_password, delete_user
-from .functions import get_asks, push_ask, delete_ask, get_user_websites, delete_user_webiste, push_user_website, get_websites, push_website, delete_website
+# functions of websites
+from .functions import get_user_websites, delete_user_webiste, push_user_website, get_websites, push_website, delete_website
+# functions of asks
+from .functions import get_asks, push_ask, delete_ask
+# functions of notices
 from .functions import get_notices, push_notice, get_num_notice, delete_notice
+# functions for admin
 from .functions import get_admin_profile
--- app/connectDB/functions.py
+++ app/connectDB/functions.py
@@ -1,9 +1,5 @@
 import pymysql
-
-from .db_email import *
-from .db_user import *
-from .db_website import *
-from .db_notice import *
+import random
 
 from conf import DB_ID as id, DB_PWD as pwd, DB_NAME as db
 
@@ -18,83 +14,468 @@ def disconnect():
     conn = None
     
 
-## db_email ##
+## email_list ##
 def push_email(user, email, website):
-    return p_email(conn, user, email, website)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT email FROM email_list WHERE user_id = '%s', AND email = '%s'" % (user, email)
+        cursor.execute(sql)
+        
+        row = cursor.fetchall()
+        if not row:
+            cursor.close()
+            return False
+        
+                
+        sql = "INSERT INTO email_list (user_id, email, website_name) VALUES ('%s', '%s', '%s')" % (user, email, website)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        cursor.close()
+        return e
+    
+    return True
     
 def delete_email(user, email, website):
-    return d_email(conn, user, email, website)
+    try:
+        cursor = conn.cursor()
+            
+        sql = "DELETE FROM email_list where user_id = '%s' && email = '%s' && website_name = '%s'" % (user, email, website)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+    except Exception as e:
+        cursor.close()
+        return e
+    
+    return True
 
 def get_recievers(name):
-    return g_recievers(conn, name)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT email FROM email_list WHERE website_name = '%s'" % name
+        cursor.execute(sql)
+        
+        rows = cursor.fetchall()
+        cursor.close()
+        if not rows:
+            return False
+        
+        return rows
+    except Exception as e:
+        cursor.close()
+        return e
     
     
-## db_user ##
+## users ##
 def push_user(info):
-    return p_user(conn, info)
+    cursor = conn.cursor()
+    
+    try:
+        sql = "INSERT INTO users (id, passwd, email) VALUES ('%s', '%s', '%s')" % (info['id'], info['passwd'], info['email'])
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+    except Exception as e:
+        cursor.close()
+        return e
+    
+    return True
     
 def update_password(user, curr, new):
-    return u_password(conn, user, curr, new)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT id FROM users WHERE id = '%s' AND passwd = '%s'" % (user, curr)
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if not row:
+            return False
+        
+        sql = "UPDATE users SET passwd = '%s' WHERE id = '%s' AND passwd = '%s'" %(new, user, curr)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
 def update_email(user, new):
-    return u_email(conn, user, new)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "UPDATE users SET email = '%s' WHERE id = '%s'" %(new, user)
+        cursor.execute(sql) # change email in users table
+        
+        sql = "UPDATE email_list SET email = '%s' WHERE user_id = '%s'" %(new, user)
+        cursor.execute(sql) # change email in email_list table
+        
+        conn.commit()
+        cursor.close()
+        
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
     
 def get_user_info(user):
-    return g_user_info(conn, user)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        sql = "SELECT id, email, passwd FROM users where id = '%s'" % user
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        cursor.close()
+        
+        return row
+    except Exception as e:
+        cursor.close()
+        return e
 
 def id_overlap_check(id):
-    return c_id_overlap(conn, id)
+    cursor = conn.cursor()
+    
+    try:
+        sql = "SELECT id FROM users WHERE id = '%s'" % id
+        cursor.execute(sql)
+        
+        row = cursor.fetchall()
+        cursor.close()
+        
+        if not row:
+            return True
+        else:
+            return False
+        
+    except Exception as e:
+        cursor.close()
+        return e
 
 def login(userid, userpwd):
-    return c_login(conn, userid, userpwd)
+    cursor = conn.cursor()
+    
+    try:
+        sql = "SELECT id, passwd FROM users WHERE id = '%s'" % userid
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        cursor.close()
+        if not row:
+            return False
+        
+        
+        if row[0] == userid and row[1] == userpwd:
+            return True
+        else:
+            return False
+    except Exception as e:
+        cursor.close()
+        return e
 
 def find_password(id, email):
-    return f_password(conn, id, email)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "select id from users where id = '%s' and email = '%s'" % (id, email)
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if not row:
+            return False
+        
+        newPass = 'noticealarm' + str(random.randint(0, 10000))
+        sql = "update users set passwd = '%s' where id = '%s' and email = '%s'" % (newPass, id, email)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+        return newPass
+    except:
+        cursor.close()
+        return 'error'
 
 def delete_user(user, password):
-    return d_user(conn, user, password)
+    try:
+        cursor = conn.cursor()
+        sql = "select id from users where id = '%s' and passwd = '%s'" % (user, password)
+        
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        
+        if not row:
+            return False
+        
+        sql = "delete from users where id = '%s' and passwd = '%s'" %(user, password)
+        cursor.execute(sql)
+        sql = "delete from email_list where user_id = '%s'" % user
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        return True
+    except:
+        return 'error'
 
-## db_website ##
+## asks ##
 def get_asks():
-    return g_asks(conn)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        sql = "SELECT name, url FROM asks ORDER BY num DESC"
+        cursor.execute(sql)
+        
+        row = cursor.fetchall()
+        cursor.close()
+        
+        return row
+    except Exception as e:
+        cursor.close()
+        return e
 
 def push_ask(name, url):
-    return p_ask(conn, name, url)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT url FROM asks WHERE url = '%s'" % url
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if row:
+            return False
+        
+        sql = "INSERT INTO asks (name, url) VALUES ('%s', '%s')" %(name, url)
+        cursor.execute(sql)
+        conn.commit()
+        
+        cursor.close()
+    except Exception as e:
+        cursor.close()
+        return e
+    
+    return True
 
 def delete_ask(url):
-    return d_ask(conn, url)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT name FROM asks WHERE url = '%s'" %url
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if not row:
+            return False
+        
+        sql = "DELETE FROM asks WHERE url = '%s'" %url
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
+## website ##
 def get_user_websites(user):
-    return g_user_websites(conn, user)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        sql = "SELECT website_name FROM email_list WHERE user_id = '%s' ORDER BY num DESC" % user
+        cursor.execute(sql)
+        
+        rows = cursor.fetchall()
+        cursor.close()
+        
+        return rows
+    except Exception as e:
+        cursor.close()
+        return e
 
 def delete_user_webiste(user, website):
-    return d_user_website(conn, user, website)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT website_name FROM email_list WHERE user_id = '%s' AND website_name = '%s'" %(user, website)
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if not row:
+            return False
+        
+        sql = "DELETE FROM email_list WHERE user_id = '%s' AND website_name = '%s'" %(user, website)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
 def push_user_website(user, email, name):
-    return p_user_website(conn, user, email, name)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT user_id FROM email_list WHERE user_id = '%s' AND website_name = '%s'" % (user, website)
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        
+        if row:
+            return False
+        
+        sql = "INSERT INTO email_list (user_id, email, website_name) VALUES ('%s', '%s', '%s')" %(user, email, website)
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
 def get_websites():
-    return g_websites(conn)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        sql = "SELECT name, url, class, path from websites ORDER BY num DESC"
+        cursor.execute(sql)
+        
+        rows = cursor.fetchall()
+        cursor.close()
+        
+        return rows
+    except Exception as e:
+        cursor.close()
+        return e
 
 def push_website(info):
-    return p_website(conn, info)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT name FROM websites where name = '%s'" % info['name']
+        cursor.execute(sql)
+        row = cursor.fetchone()
+        
+        if row:
+            cursor.close()
+            return False
+        
+        sql = "INSERT INTO websites (name, url, class, path) VALUES ('%s', '%s', '%s', '%s')" %(info['name'], info['url'], info['class'], info['path'])
+        cursor.execute(sql)
+        conn.commit()
+        
+        cursor.close()
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
-def delete_website(info):
-    return d_website(conn, info)
+def delete_website(name):
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT name FROM websites WHERE name='%s'" % name
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        if not row:
+            cursor.close()
+            return False
+        
+        sql = "DELETE FROM websites WHERE name='%s'" % name
+        cursor.execute(sql)
+        sql = "DELETE FROM email_list WHERE website_name='%s'" %name
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+
+        return True
+    except Exception as e:
+        cursor.close()
+        return e
 
 
 ## db_notice ##
 def get_notices():
-    return g_notices(conn)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        
+        sql = "SELECT num, title, writer, time from notices ORDER BY num DESC"
+        cursor.execute(sql)
+        
+        row = cursor.fetchall()
+        cursor.close()
+        
+        return row
+    except Exception as e:
+        cursor.close()
+        return e
 
 def get_num_notice(num):
-    return g_num_notice(conn, num)
+    try:
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        sql = "SELECT title, content, time FROM notices WHERE num = '%s'" %num
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        cursor.close()
+        return row
+    except Exception as e:
+        cursor.close()
+        return e
 
 def push_notice(title, content, time):
-    return p_notice(conn, title, content, time)
+    try:
+        cursor = conn.cursor()
+        sql = "INSERT INTO notices (title, content, writer, time) VALUES ('%s', '%s', 'admin', '%s')" %(title, content, time)
+        
+        cursor.execute(sql)
+        conn.commit()
+        
+        cursor.close()
+    except Exception as e:
+        cursor.close()
+        return e
+    
+    return True
 
 def delete_notice(num):
-    return d_notice(conn, num)
+    try:
+        cursor = conn.cursor()
+        
+        sql = "SELECT num FROM notices WHERE num = '%s'" %num
+        cursor.execute(sql)
+        
+        row = cursor.fetchone()
+        
+        if not row:
+            return False
+        
+        sql = "DELETE FROM notices WHERE num = '%s'" %num
+        cursor.execute(sql)
+        
+        conn.commit()
+        cursor.close()
+        
+        return True
+    except Exception as e:
+        return e
 
 ## for admin ##
 def get_admin_profile():
